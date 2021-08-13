10a567d7b522c7f4e27aeea773dde80c0f869f58
Nguyễn Nhật Quang <36161147+QuangNhatTewak@users.noreply.github.com>
2021-08-13 17:35:02 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,67 @@
+function hide() {
+    const x = document.getElementById("but3");
+    const y = document.getElementById("but2");
+     x.style.display="block";
+     y.style.display="none";
+     document.getElementById("audio2").play();
+    
+}
+function hide2() {
+    const x = document.getElementById("but3");
+    const y = document.getElementById("but4");
+     x.style.display="none";
+     y.style.display="block";
+     document.getElementById("audio2").play();
+}
+function hide3() {
+    const x = document.getElementById("but4");
+    const y = document.getElementById("but2");
+     x.style.display="none";
+     y.style.display="none";
+     document.getElementById("audio2").play();
+}
+function show() {
+    const x = document.getElementById("click");
+    const y = document.getElementById("form1");
+     x.style.display="none";
+     y.style.display="block";
+
+    
+}
+function troll() {
+    const x = document.getElementById("fbut2");
+    const y = document.getElementById("fbut3");
+      x.style.display="none";
+      y.style.display="block";
+      document.getElementById("alert").innerHTML = "Cậu đừng có dối lòng nữa 😙 Hãy nói thật đi";
+      document.getElementById("alert2").innerHTML = "Cậu mà thoát Web hay Block tớ là cậu đồng ý nha 😛";
+      document.getElementById("audio2").play();
+      return false;
+}
+function troll1() {
+    const x = document.getElementById("fbut3");
+    const y = document.getElementById("fbut4");
+     x.style.display="none";
+     document.getElementById("audio2").play();
+     y.style.display="block";
+     return false;
+}
+function troll2() {
+    const x = document.getElementById("fbut2");
+    const y = document.getElementById("fbut4");
+     x.style.display="none";
+     y.style.display="none";
+     document.getElementById("audio2").play();
+     return false;
+}
+function inrel() {
+    const x = document.getElementById("form1");
+    const y = document.getElementById("yess");
+    const z = document.getElementById("click");
+     x.style.display="none";
+     y.style.display="block";
+     z.style.display="none";
+     document.getElementById("audio").play();
+     return false;
+
+}
