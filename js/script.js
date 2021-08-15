7fa46d22400327c23184b8df4276acdce21383b0
Nguyễn Nhật Quang <36161147+QuangNhatTewak@users.noreply.github.com>
2021-08-15 18:57:16 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ function troll() {
     const y = document.getElementById("fbut3");
       x.style.display="none";
       y.style.display="block";
-      document.getElementById("alert").innerHTML = "Cậu đừng có dối lòng nữa 😙 Hãy nói thật đi";
+      document.getElementById("alert").innerHTML = "😍";
       document.getElementById("alert2").innerHTML = "Cậu mà thoát trang Web này hay Block tớ là cậu đồng ý nha 😛";
       document.getElementById("audio2").play();
       return false;
